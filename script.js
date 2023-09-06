638cdd7959c46b949a0f72e1d6fb9981450a9403
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-06 19:36:44 -0400

    trying to change padding.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
-const print_l = () => {
-    if (window.innerWidth < "400px") {
-    let nav_bar = document.querySelector("nav");
-    let nav_list = document.querySelector(".nav_list");
-    nav_bar.removeChild(nav_list);
+let nav_list = document.querySelector(".nav_list");
+if (window.innerWidth < 400) {
+    if (nav_list) {
+        nav_list.remove();
     }
-}
+    let main = document.querySelector(main);
+    main.style = "padding: 20px";
+    let nav_bar = doucment.querySelector(nav);
+    nav_bar.style = "margin-left: 20px"
 
-print_l();
+}

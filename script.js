724350fefb7536306efc675e37b33ac4049cf461
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-06 18:49:19 -0400

    trying to make the nav_bar responsive for mobile.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-if (window.innerWidth <= "400px") {
+
+const print_l = () => {
+    if (window.innerWidth < "400px") {
+    let nav_bar = document.querySelector("nav");
     let nav_list = document.querySelector(".nav_list");
-    console.log(nav_list.innerHTML);
+    nav_bar.removeChild(nav_list);
+    }
 }
 
+print_l();

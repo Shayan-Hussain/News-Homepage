63c4c0713cdb726ded6fbd4d77233e102bc6e951
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-08 20:42:42 -0400

    added a hamburger icon.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const res = () => {
-    if (window.innerWidth <= 400) {
-        if (nav_list) {
-            let nav_list = document.querySelector(".nav_list");
-            nav_list
-        }
-    }
-}
 
-res();
+let nav_list = document.querySelector(".nav_list");
+let ham_img = `<img src="/assets/icon-menu.svg" alt="menu" class="ham_burger_icon">`
+
+if (window.innerWidth < 380) {
+    if (nav_list)   {
+        nav_list.remove();
+        let nav = document.querySelector("nav");
+        nav.innerHTML += ham_img;
+    }
+    
+}

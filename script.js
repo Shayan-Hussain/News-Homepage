2e09d178b5b3c95a471a37109069daceee524d8c
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-09 21:03:37 -0400

    added jQuery for navbar. Trying to undo the change

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 
+let nav = document.querySelector("nav");
 let nav_list = document.querySelector(".nav_list");
+let nav_list_string = nav_list.innerHTML;
 let ham_img = `<img src="/assets/icon-menu.svg" alt="menu" class="ham_burger_icon">`
+let screen_minized = false;
 
-if (window.innerWidth < 380) {
-    if (nav_list)   {
-        nav_list.remove();
-        let nav = document.querySelector("nav");
-        nav.innerHTML += ham_img;
+function list_ham_changer() {
+    if (window.innerWidth < 380 && !screen_minized) {
+        if (nav_list)   {
+            nav_list.remove();
+            nav.innerHTML += ham_img;
+            screen_minized = true;
+        }
+    } else {
+        if (!nav_list) {
+        nav.innerHTML += nav_list_string;
+        }
     }
 }
+
+list_ham_changer();
+
+$(window).resize(function() {
+    list_ham_changer();
+});

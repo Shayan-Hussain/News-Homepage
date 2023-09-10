e61b181acf566605c55c27b3859c70ba09bc64ce
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-10 15:06:49 -0400

    added the mobile navigation menu.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let nav_list_string =
             <li><a class="list_item_ahcr">Popular</a></li>
             <li><a class="list_item_ahcr">Catagories</a></li>
         </ul>`
-let ham_img = `<img src="/assets/icon-menu.svg" alt="menu" class="ham_burger_icon">`
+let ham_img = `<img src="/assets/icon-menu.svg" alt="menu" class="ham_burger_icon" onclick="show_menu()">`
 let screen_minized = false;
 
 function list_ham_changer() {
@@ -33,16 +33,40 @@ $(window).resize(function() {
     list_ham_changer();
 });
 
-function show_menu() {
-    let mobile_nav = 
-    `<div class="mobile_nav">
-        <a class="ham_list">Home</a>
-        <a class="ham_list">New</a>
-        <a class="ham_list">Trending</a>
-        <a class="ham_list">Popular</a>
-        <a class="ham_list">Catagories</a>
-    </div>`
+let open = false;
 
-    nav.innerHTML += mobile_nav;
-    
+function show_menu() {
+
+    if (open === false) {
+
+        nav.innerHTML = "";
+        nav.style = "flex-direction: column;";
+
+        let mobile_logo_cross =
+        `<div class="mobile_logo_cross" style="margin: 10px 0;">
+        <a class="logo_ahcr"><img src="/assets/logo.svg" alt="W." class="logo"></a>
+        <img src="/assets/icon-menu-close.svg" alt="menu" class="ham_burger_icon" onclick="show_menu()">
+        </div>`
+
+        let mobile_nav = 
+        `<div class="mobile_nav">
+            <a class="ham_list border_top">Home</a>
+            <a class="ham_list">New</a>
+            <a class="ham_list">Trending</a>
+            <a class="ham_list">Popular</a>
+            <a class="ham_list">Catagories</a>
+        </div>`
+
+        nav.innerHTML += mobile_logo_cross;
+        nav.innerHTML += mobile_nav;
+        open = true;
+        return;
+    }   else    {
+        nav.style = "flex-direction: flex-end;";
+        let mobile_nav  = document.querySelector(".mobile_nav");
+        mobile_nav.remove();
+        open = false;
+        return;
+    }
+
 }

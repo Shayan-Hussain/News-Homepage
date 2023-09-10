c64578fafb99312713a9c139b8231180bdafa08f
Shayan Hussain <shayan.hussainzardari@gmail.com>
2023-09-09 21:17:54 -0400

    trying to add the menu items in mobile.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
 let nav = document.querySelector("nav");
 let nav_list = document.querySelector(".nav_list");
-let nav_list_string = nav_list.innerHTML;
+let nav_list_string = 
+        `<ul class="nav_list">
+            <li><a class="list_item_ahcr">Home</a></li>
+            <li><a class="list_item_ahcr">New</a></li>
+            <li><a class="list_item_ahcr">Trending</a></li>
+            <li><a class="list_item_ahcr">Popular</a></li>
+            <li><a class="list_item_ahcr">Catagories</a></li>
+        </ul>`
 let ham_img = `<img src="/assets/icon-menu.svg" alt="menu" class="ham_burger_icon">`
 let screen_minized = false;
 
@@ -14,6 +21,7 @@ function list_ham_changer() {
         }
     } else {
         if (!nav_list) {
+        nav.innerHTML += ham_img;
         nav.innerHTML += nav_list_string;
         }
     }
@@ -24,3 +32,17 @@ list_ham_changer();
 $(window).resize(function() {
     list_ham_changer();
 });
+
+function show_menu() {
+    let mobile_nav = 
+    `<div class="mobile_nav">
+        <a class="ham_list">Home</a>
+        <a class="ham_list">New</a>
+        <a class="ham_list">Trending</a>
+        <a class="ham_list">Popular</a>
+        <a class="ham_list">Catagories</a>
+    </div>`
+
+    nav.innerHTML += mobile_nav;
+    
+}
